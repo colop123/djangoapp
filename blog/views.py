ec9ce4fe88a42edcd72e580de35c2ae5b7a20419
Samuel Colop <smaylopco@yahoo.es>
2016-09-30 10:04:57 -0600

    se aplico un redireccionamiento a los titulos de la pagina utilizando una plantilla base

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Postear
 
 def listar_articulo(request):
     posts= Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_articulo.html', {'posts': posts})
+
+def detalle_articulo(request, pk):
+        post = get_object_or_404(Postear, pk=pk)
+        return render(request, 'blog/detalle_articulo.html', {'post': post})
