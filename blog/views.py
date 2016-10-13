b358146035e7afd7bc308692d7789f3168c6f300
Samuel Colop <smaylopco@yahoo.es>
2016-10-13 12:29:12 -0600

    se creo la opcion de agregar post y edicion de los mismos

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Postear
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def listar_articulo(request):
     posts= Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -9,3 +11,29 @@ def listar_articulo(request):
 def detalle_articulo(request, pk):
         post = get_object_or_404(Postear, pk=pk)
         return render(request, 'blog/detalle_articulo.html', {'post': post})
+
+def post_new(request):
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.fecha_publicacion = timezone.now()
+                post.save()
+                return redirect('blog.views.detalle_articulo', pk=post.pk)
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+        post = get_object_or_404(Postear, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('blog.views.detalle_articulo', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
